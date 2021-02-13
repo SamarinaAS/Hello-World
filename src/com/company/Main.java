8234c3a686aff969ec606d8803c091a7ae462f14
Anastasia Samarina <a.samarina1997@yandex.ru>
2021-02-13 18:49:27 +0300

    add java "Hello World"

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int i = 1;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        System.out.println("Hello World!");
+    }
+}
